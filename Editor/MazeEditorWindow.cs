18f4d0ed2264e4b2aa1e5e8c9ff2a4b8f4afbcbd
Markus Fleck <fleckmarkus.privat@gmail.com>
2015-02-15 22:51:57 +0100

    Maze Generation finished

--- Editor/MazeEditorWindow.cs
+++ Editor/MazeEditorWindow.cs
@@ -10,12 +10,13 @@ public class MazeEditorWindow : EditorWindow
     bool CreateWaypoints = true;
     
     float WallThickness = 0.1f;
+    float WallLength = 1.4f;
     Vector2 UnitDimension = new Vector2(1.5f,1.5f);
     float mazeWidth = 0;
     float mazeLength = 0;
 
-    int unitsInLength = 0;
-    int unitsInWidth = 0;
+    int unitRows = 0;
+    int unitColumns = 0;
 
     private beMobileMaze selectedMaze;
 
@@ -66,6 +67,7 @@ public class MazeEditorWindow : EditorWindow
 
             wallPrototypeEditor.OnPreviewGUI(GUILayoutUtility.GetRect(150, 150), EditorStyles.whiteLabel);
         }
+
         
         GUILayout.Label("Edge prototype", EditorStyles.boldLabel);
         edgePrototype = (GameObject)EditorGUILayout.ObjectField(edgePrototype, typeof(GameObject), true);
@@ -91,16 +93,17 @@ public class MazeEditorWindow : EditorWindow
         }
 
         if (GUILayout.Button("Configure Maze"))
-            {
-                UpdateTheSelectedMaze();
-            }
+        {
+            UpdateTheSelectedMaze();
+        }
+        
         EditorGUILayout.EndVertical();
     }
 
     private void UpdateTheSelectedMaze(){
 
-        unitsInWidth = Mathf.FloorToInt( mazeWidth / UnitDimension.x);
-        unitsInLength = Mathf.FloorToInt( mazeLength / UnitDimension.y);
+        unitColumns = Mathf.FloorToInt( mazeWidth / UnitDimension.x);
+        unitRows = Mathf.FloorToInt( mazeLength / UnitDimension.y);
 
         if ((selectedMaze.Walls == null && selectedMaze.Edges == null) || selectedMaze.Edges.Count == 0 && selectedMaze.Walls.Count == 0)
             {
@@ -122,40 +125,98 @@ public class MazeEditorWindow : EditorWindow
 
         Vector2 unitDimension = UnitDimension;
 
-        float starPointX = mazeHost.transform.position.x - selectedMaze.MazeWidthInMeter / 2;
-        float startPointY = mazeHost.transform.position.z - selectedMaze.MazeLengthInMeter / 2;
-        Vector3 startPosition =  new Vector3(starPointX, 0, startPointY);
+        float starPointX = mazeHost.transform.position.x - WallThickness / 2 - selectedMaze.MazeWidthInMeter / 2;
+        float startPointY = mazeHost.transform.position.z - WallThickness / 2 - selectedMaze.MazeLengthInMeter / 2;
+        float floor = mazeHost.transform.position.y;
+        Vector3 startPosition =  new Vector3(starPointX, floor, startPointY);
  
-        for (int i = 0; i < unitsInWidth; i++)
+        for (int col = 0; col < unitColumns; col++)
         {
-            for (int j = 0; j < unitsInLength; j++)
-            {  
-                Vector3 unitPosition = startPosition + new Vector3(i * unitDimension.x, 0, j * unitDimension.y);
-                GameObject unit = CreateUnitWithParentAt(string.Format(UnitNamePattern, i, j), selectedMaze.transform, unitPosition);
+            for (int row = 0; row < unitRows; row++)
+            {
+                Vector3 unitPosition = startPosition + new Vector3(col * unitDimension.x, floor, row * unitDimension.y);
+                GameObject unit = CreateUnitWithParentAt(string.Format(UnitNamePattern, col, row), selectedMaze.transform, unitPosition);
                 mazeHost.Units.Add(unit);
+                 
+                /** Create between row & col
+                 * 
+                 *  |
+                 *  #--
+                 * 
+                 **/
+
+                Vector3 targetPosition = unit.transform.position;
+                Vector3 targetColumnWallPosition = targetPosition + new Vector3(WallThickness, floor, WallThickness);
+                Vector3 targetRowWallPosition = targetPosition + new Vector3(0, floor, WallThickness);
+
+
+                GameObject edge = (GameObject)GameObject.Instantiate(edgePrototype, targetPosition, Quaternion.identity);
+                edge.transform.parent = unit.transform;
+                GameObject colWall = (GameObject)GameObject.Instantiate(wallPrototype, targetColumnWallPosition,  Quaternion.Euler(0,90,0));
+                colWall.transform.parent = unit.transform;
+
+                GameObject rowWall = (GameObject)GameObject.Instantiate(wallPrototype, targetRowWallPosition, Quaternion.identity);
+                rowWall.transform.parent = unit.transform;
+                
+                /** Create last column n-1
+                 * 
+                 * --#
+                 *   |
+                 * --#
+                 *   |
+                 * --#
+                 *   
+                 **/
+                if (col == unitColumns -1)
+                {   
+                    Vector3 targetLastColumnEdgePosition = targetColumnWallPosition + new Vector3(WallLength, floor, -WallThickness);
+
+                    GameObject lastColedge = (GameObject)GameObject.Instantiate(edgePrototype, targetLastColumnEdgePosition, Quaternion.identity);
+                    lastColedge.transform.parent = unit.transform;
+
+                    Vector3 targetLastColumnWallPosition = targetLastColumnEdgePosition + new Vector3(0, floor, WallThickness);
+                    GameObject lastColWall = (GameObject)GameObject.Instantiate(wallPrototype, targetLastColumnWallPosition, Quaternion.identity);
+                    lastColWall.transform.parent = unit.transform;
+
+                    if (row == unitRows - 1) { 
+                        Vector3 targetLastColumRowEdge = targetLastColumnWallPosition + new Vector3(0, floor, WallLength);
+                        GameObject lastColRowEdge = (GameObject)GameObject.Instantiate(edgePrototype, targetLastColumRowEdge, Quaternion.identity);
+                        lastColRowEdge.transform.parent = unit.transform;
+                    }
+                }
+
+               /** Create last row n-1
+                * 
+                * #--#--#
+                * |  |  |
+                * #--#--#
+                * |  |  |
+                **/
+                if (row == unitRows - 1)
+                {
+                    Debug.Log("column " + col + " last row reached");
+                    Vector3 lastEdgePosition = targetPosition + new Vector3(0, floor, WallLength + WallThickness);
+                    GameObject lastEdge = (GameObject)GameObject.Instantiate(edgePrototype, lastEdgePosition, Quaternion.identity);
+                    lastEdge.transform.parent = unit.transform; 
+
+                    Vector3 lastWallPosition = lastEdgePosition + new Vector3(WallThickness, 0, WallThickness);
+                    GameObject lastRowWall = (GameObject)GameObject.Instantiate(wallPrototype, lastWallPosition, Quaternion.Euler(0, 90, 0));
+                    lastRowWall.transform.parent = unit.transform;
+                }
+
             }
         }
         
         return mazeHost;
     }
 
-    private GameObject CreateUnitWithParentAt(string name, Transform parent, Vector3 position) 
+    private GameObject CreateUnitWithParentAt(string name, Transform parent, Vector3 position)
     {
-         GameObject unit = new GameObject(name);
-                unit.transform.parent = parent;
-                unit.transform.position = position; 
-                return unit;
-    }
-
-    private object CreateNewEdgeAt(float ColOffset, float RowOffset)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    private object CreateNewWallAt(float ColOffset, float RowOffset)
-    {
-        throw new System.NotImplementedException();
-    }
+        GameObject unit = new GameObject(name);
+        unit.transform.parent = parent;
+        unit.transform.position = position;
+        return unit;
+    } 
 
     
 }
