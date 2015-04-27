0ceba16bf9aa139d7f485efecf40a903a5afcda0
Markus Fleck <fleckmarkus.privat@gmail.com>
2015-04-27 12:36:07 +0200

    adding first version of a path model

---
+++ Scripts/Maze/PathInMaze.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PathInMaze : ScriptableObject {
+
+    [SerializeField]
+    public LinkedList<Vector2> GridIDs;
+
+    public void Setup(LinkedList<MazeUnit> pathInSelection)
+    {
+        GridIDs = new LinkedList<Vector2>();
+
+        foreach (var item in pathInSelection)
+        {
+            GridIDs.AddLast(item.GridID);
+        }
+
+    }
+}
