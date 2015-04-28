3c7ed806f0ee06e6ee34332b898435fa335ed511
Markus Fleck <fleckmarkus.privat@gmail.com>
2015-04-28 11:26:33 +0200

    Adding first approach to encapsulate paradigm logic

---
+++ Example/MultiMazePathRetrieval.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public enum ParadigmCondition { Active, Passive }
+
+public class MultiMazePathRetrieval : MonoBehaviour {
+
+	public VirtualRealityManager environment;
+	public HUDInstruction instructions;
+	public DebugMarkerStream markerStream;
+
+	public ITrial currentTrial;
+
+	[SerializeField]
+	public int NumberOfTrainingsTrials;
+
+	[SerializeField]
+	public int NumberOfTrialsPerCondition;
+
+	void Awake()
+	{
+		if (environment == null)
+			throw new MissingReferenceException("Reference to VirtualRealityManager is missing");
+
+		if (markerStream == null)
+			throw new MissingReferenceException("Reference to a MarkerStream instance is missing");
+
+		if (instructions == null)
+			throw new MissingReferenceException("No HUD available, you are not able to give visual instructions");
+	}
+
+	// Use this for initialization
+	void Start () {
+		 
+		currentTrial = new Training(environment, markerStream, instructions);
+		currentTrial.Initialize();
+
+        currentTrial.OnEnd += currentTrial_OnEnd;
+	}
+
+    void currentTrial_OnEnd()
+    {
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Example/Training.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public interface ITrial
+{
+    void Initialize();
+
+    event Action OnStart;
+
+    event Action OnEnd;
+
+    void CleanUp();
+}
+
+public class Training : ITrial {
+
+    /// <summary>
+    /// Könnten auch Eigenschaften einer Basisklasse sein
+    /// </summary>
+    VirtualRealityManager environment;
+
+    IMarkerStream marker;
+
+    HUDInstruction hud;
+
+    beMobileMaze mazeControl;
+
+    PathInMaze path;
+
+    public Training(VirtualRealityManager vrmanager, IMarkerStream streamInterface, HUDInstruction instructions)
+    {
+        environment = vrmanager;
+        marker = streamInterface;
+        hud = instructions;
+    }
+
+    public void Initialize()
+    {
+        // Idea
+        // var targetWorld = string.Format("maze{0}", Random.Range(0,1));
+        // var targetPath = string.Format("path{0}", Random.Range(0,1));
+
+        var targetWorld = "maze";
+        var targetPath = "pathA";
+
+        environment.ChangeWorld(targetWorld);
+
+        mazeControl = environment.ActiveEnvironment.GetComponent<beMobileMaze>();
+
+        path = mazeControl.RequirePath(targetPath);
+
+        mazeControl.MazeUnitEventOccured += mazeControl_MazeUnitEventOccured;
+        
+    }
+
+    /// <summary>
+    /// A Trial Start may caused from external source (e.g. a key press)
+    /// </summary>
+    public void Start()
+    {
+        hud.StartDisplaying("TrainingsInstruction");
+
+        if (OnStart != null)
+            OnStart();
+    }
+
+    public event Action OnStart;
+
+    void mazeControl_MazeUnitEventOccured(MazeUnitEvent obj)
+    {
+        if (obj.MazeUnitEventType == MazeUnitEventType.Entering)
+        {
+            Debug.Log("Training Trial Instance recieved MazeUnitEvent: Entering");
+        }
+
+        if (obj.MazeUnitEventType == MazeUnitEventType.Exiting)
+        {
+            Debug.Log("Training Trial Instance recieved MazeUnitEvent: Exiting");
+        }
+    }
+
+    public event Action OnEnd;
+
+    public void CleanUp()
+    {
+        mazeControl.MazeUnitEventOccured -= mazeControl_MazeUnitEventOccured;
+    }
+
+}
