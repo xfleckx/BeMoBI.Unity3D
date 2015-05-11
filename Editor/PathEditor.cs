ef9459a5e0f5efd0999562c0c35aa59b01842e1a
Markus Fleck <fleckmarkus.privat@gmail.com>
2015-05-11 10:19:36 +0200

    Adding basic structure of path component

---
+++ Editor/PathEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(PathController))]
+public class PathEditor : Editor {
+
+    PathController instance;
+    beMobileMaze hostMaze;
+    void OnInspectorGUI()
+    {
+        instance = target as PathController;
+
+        if (instance != null) hostMaze = instance.GetComponentInParent<beMobileMaze>();
+            
+        if(hostMaze == null) throw new MissingComponentException(string.Format("The Path Controller should be attached to a {0} instance", typeof(beMobileMaze).Name)); 
+    }
+
+}
