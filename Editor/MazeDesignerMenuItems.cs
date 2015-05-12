5085eb7b11f91bb599f44155ddcd2383b8e3e649
Markus Fleck <fleckmarkus.privat@gmail.com>
2015-05-12 14:26:29 +0200

    preparing MazeModel for later use

--- Editor/MazeDesignerMenuItems.cs
+++ Editor/MazeDesignerMenuItems.cs
@@ -6,9 +6,10 @@ public class MazeDesignerMenuItems : ScriptableObject
     [MenuItem("beMobile/MazeDesigner/Add new Maze")]
     static void CreateNewMazeInScene()
     {
-        GameObject maze = new GameObject("beMobilMaze");
-        var mazeController = maze.AddComponent<beMobileMaze>();
-        mazeController.Grid = MazeEditorUtil.ReconfigureGrid(mazeController, mazeController.MazeLengthInMeter, mazeController.MazeWidthInMeter);
+        GameObject mazeHost = new GameObject("beMobilMaze");
+        var maze = mazeHost.AddComponent<beMobileMaze>();
+        var mazeModel = ScriptableObject.CreateInstance<MazeModel>();
+        maze.Grid = MazeEditorUtil.ReconfigureGrid(maze, maze.MazeLengthInMeter, maze.MazeWidthInMeter);
     }
 
     [MenuItem("beMobile/MazeDesigner/Open Editor on new Maze")]
