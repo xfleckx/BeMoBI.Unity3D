4954a004da98e705bdbed78b4580b3816e7858b2
Markus Fleck <fleckmarkus.privat@gmail.com>
2015-05-15 22:30:49 +0200

    Reduce code in that class, make Rows and colums read-only

--- Scripts/Maze/beMobileMaze.cs
+++ Scripts/Maze/beMobileMaze.cs
@@ -22,10 +22,10 @@ public class beMobileMaze : MonoBehaviour
     public Vector2 EdgeDimension = new Vector2(0.1f, 0.1f);
     [SerializeField]
     public float WallThicknessInMeter = 0.1f;
-    [SerializeField]
-	public int Rows;
-    [SerializeField]
-	public int Columns;
+
+    public int Rows { get { return Grid != null ? Grid.GetLength(1) : 0; } }
+
+    public int Columns { get { return Grid != null ? Grid.GetLength(0) : 0; } }
 
 	public List<GameObject> Walls;
 	public List<GameObject> Edges;
@@ -39,32 +39,6 @@ public class beMobileMaze : MonoBehaviour
     
     [SerializeField]
     public MazeUnit[,] Grid; 
-
-	public LinkedList<MazeUnit> CreatePathFromGridIDs(LinkedList<Vector2> gridIDs)
-	{
-		var enumerator = gridIDs.GetEnumerator();
-		var units = new LinkedList<MazeUnit>();
-
-		while (enumerator.MoveNext()) {
-
-			var gridField = enumerator.Current;
-
-			var correspondingUnitHost = transform.FindChild(string.Format("Unit_{0}_{1}", gridField.x, gridField.y));
-
-			if (correspondingUnitHost == null)
-				throw new MissingComponentException("It seems, that the path doesn't match the maze! Requested Unit is missing!");
-		
-			var unit = correspondingUnitHost.GetComponent<MazeUnit>();
-
-			if (unit == null)
-				throw new MissingComponentException("Expected Component on Maze Unit is missing!");
-
-			units.AddLast(unit);
-		}
-
-		return units;
-
-	}
 
 	public void RecieveUnitEvent(MazeUnitEvent unitEvent)
 	{
@@ -87,77 +61,15 @@ public class beMobileMaze : MonoBehaviour
 #endif
 
 	public void OnDrawGizmos()
-	{
-		drawFloorGrid();
+	{ 
 #if UNITY_EDITOR
 		if (EditorGizmoCallbacks != null)
 			EditorGizmoCallbacks();
 #endif
 	}
-
-	private void drawFloorGrid()
-	{
-		// store map width, height and position
-		var mapWidth = MazeWidthInMeter;
-		var mapHeight = MazeLengthInMeter;
-		var position = this.transform.position;
-
-		// draw layer border
-		Gizmos.color = Color.white;
-		Gizmos.DrawLine(position, position + new Vector3(mapWidth, 0, 0));
-		Gizmos.DrawLine(position, position + new Vector3(0, 0, mapHeight));
-		Gizmos.DrawLine(position + new Vector3(mapWidth, 0, 0), position + new Vector3(mapWidth, 0, mapHeight));
-		Gizmos.DrawLine(position + new Vector3(0, 0, mapHeight), position + new Vector3(mapWidth, 0, mapHeight));
-
-		Columns = Mathf.FloorToInt(MazeWidthInMeter / RoomDimension.x);
-		Rows = Mathf.FloorToInt(MazeLengthInMeter / RoomDimension.z);
-
-		// draw tile cells
-		Gizmos.color = Color.grey;
-		for (float i = 1; i < Columns + 1; i++)
-		{
-			Gizmos.DrawLine(position + new Vector3(i * RoomDimension.x, 0, 0), position + new Vector3(i * RoomDimension.x, 0, mapHeight));
-		}
-
-		for (float i = 1; i < Rows + 1; i++)
-		{
-			Gizmos.DrawLine(position + new Vector3(0, 0, i * RoomDimension.z), position + new Vector3(mapWidth, 0, i * RoomDimension.z));
-		}
-
-	}
-
-    public void UpdateGrid()
-    {
-        var existingUnits = GetComponentsInChildren<MazeUnit>();
-
-        foreach (var unit in existingUnits)
-        {
-            if (unit.GridID.x >= Columns)
-                Columns = Mathf.FloorToInt(unit.GridID.x);
-
-            if (unit.GridID.y >= Rows)
-                Rows = Mathf.FloorToInt(unit.GridID.y);
-
-            Units.Add(unit);
-        }
-
-        Columns += 1;
-        Rows += 1;
-
-        Grid = new MazeUnit[Columns, Rows];
-
-        foreach (var unit in Units)
-        {
-            var x = Mathf.FloorToInt(unit.GridID.x);
-            var y = Mathf.FloorToInt(unit.GridID.y);
-            Grid[x, y] = unit;
-        }
-    }
-
+     
     void Reset()
     {
-        Debug.Log("Reset on Maze");
-
-        UpdateGrid();
+        Debug.Log("Reset on Maze"); 
     }
 }
