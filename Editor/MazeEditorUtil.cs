386c5249d0e5ea5a7c155505d79ce38daa9242d0
Markus Fleck <fleckmarkus.privat@gmail.com>
2015-05-16 19:25:08 +0200

    minor changes - class may become deprecated

--- Editor/MazeEditorUtil.cs
+++ Editor/MazeEditorUtil.cs
@@ -6,63 +6,58 @@ using UnityEngine;
 
 public static class MazeEditorUtil
 {
-    public static MazeUnit[,] ReconfigureGrid(beMobileMaze maze,  int newColumns, int newRows)
+    /// <summary>
+    /// Check if there are already unitsFound existing and put them into the new Grid
+    /// </summary>
+    /// <param name="maze"></param>
+    /// <param name="newColumns"></param>
+    /// <param name="newRows"></param>
+    /// <returns></returns>
+    public static MazeUnit[,] ReconfigureGrid(beMobileMaze maze, int newColumns, int newRows)
     {
-        if (maze.Grid != null) { 
+        MazeUnit[,] newGrid = new MazeUnit[newColumns, newRows];
 
-            MazeUnit[,] newGrid = new MazeUnit[newColumns, newRows];
-
-            for (int col = 0; col < newColumns; col++)
+        for (int col = 0; col < newColumns; col++)
+        {
+            for (int row = 0; row < newRows; row++)
             {
-                for (int row = 0; row < newRows; row++)
-                {
-                    var hostGameObject = GameObject.Find(string.Format(maze.UnitNamePattern, col, row));
-                    
-                    if(hostGameObject != null)
-                        newGrid[col, row] = hostGameObject.GetComponent<MazeUnit>();
-                }
+                var hostGameObject = GameObject.Find(string.Format(maze.UnitNamePattern, col, row));
+
+                if (hostGameObject != null)
+                    newGrid[col, row] = hostGameObject.GetComponent<MazeUnit>();
             }
+        }
 
-            maze.Grid = newGrid;
+        maze.Grid = newGrid;
 
-            var unitsOutsideOfGrid = maze.Units.Select(
-                (u) => {
+        var unitsOutsideOfGrid = maze.Units.Select(
+            (u) =>
+            {
 
-                    if (u.GridID.x > newColumns || u.GridID.y > newRows)
-                        return u.gameObject;
-                    else
-                        return null;
-                });
+                if (u.GridID.x > newColumns || u.GridID.y > newRows)
+                    return u.gameObject;
+                else
+                    return null;
+            });
 
-            unitsOutsideOfGrid.ImmediateDestroyObjects();
+        unitsOutsideOfGrid.ImmediateDestroyObjects();
 
-            return maze.Grid;
-
-        } 
-        
-
-
-        maze.Grid = new MazeUnit[newColumns, newRows];
-        
         return maze.Grid;
     }
-
-
-
-    public static MazeUnit[,] ReconfigureGrid(beMobileMaze maze,  float columns, float rows)
+     
+    public static MazeUnit[,] ReconfigureGrid(beMobileMaze maze, float columns, float rows)
     {
         return ReconfigureGrid(maze,Mathf.FloorToInt(columns), Mathf.FloorToInt(rows));
     }
-
-
+     
     public static void ImmediateDestroyObjects(this IEnumerable<GameObject> set)
     {
-        if (set.Any())
-        {
-            var first = set.First();
-            var tail = set.Skip(1);
-            GameObject.DestroyImmediate(first);
-            tail.ImmediateDestroyObjects();
-        }
+        if (!set.Any())
+            return;
+
+        var first = set.First();
+        var tail = set.Skip(1);
+        GameObject.DestroyImmediate(first);
+        tail.ImmediateDestroyObjects(); 
     }
 }
