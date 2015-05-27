ceee24b52697ffa2ce76387c7c99b64cdd058540
Markus Fleck <fleckmarkus.privat@gmail.com>
2015-05-27 09:09:30 +0200

    Remove deprecated Maze creation and save prefab after creation

--- Editor/MazeDesignerMenuItems.cs
+++ Editor/MazeDesignerMenuItems.cs
@@ -3,24 +3,22 @@ using UnityEditor;
 
 public class MazeDesignerMenuItems : ScriptableObject
 {
-    [MenuItem("beMobile/MazeDesigner/Add new Maze")]
-    static void CreateNewMazeInScene()
-    {
-        GameObject mazeHost = new GameObject("beMobilMaze");
-        var maze = mazeHost.AddComponent<beMobileMaze>();
-        var mazeModel = ScriptableObject.CreateInstance<MazeModel>();
-    }
-
     [MenuItem("beMobile/MazeDesigner/Add new Maze as Prefab")]
     static void CreateNewMazeAsPrefab()
     {
         var filePath = EditorUtility.SaveFilePanelInProject("Save the prefab", "aBeMoBIMaze", "prefab","Message");
+
+        if (filePath == string.Empty)
+            return;
+
         var prefab = PrefabUtility.CreateEmptyPrefab(filePath);
 
-
         GameObject mazeHost = new GameObject("beMoBIMaze");
-        PrefabUtility.ReplacePrefab(mazeHost, prefab, ReplacePrefabOptions.ConnectToPrefab);
         var maze = mazeHost.AddComponent<beMobileMaze>(); 
+        PrefabUtility.ReplacePrefab(mazeHost, prefab, ReplacePrefabOptions.ConnectToPrefab);
+
+        EditorUtility.SetDirty(prefab);
+        AssetDatabase.SaveAssets();
     }
 
     [InitializeOnLoad]
