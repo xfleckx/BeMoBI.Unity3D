fd2adc9d72c99fc6b5fe16d00f86bc898a958e87
Markus Fleck <fleckmarkus.privat@gmail.com>
2015-10-02 15:54:03 +0200

    [FEATURE] Make Grid structure visible through labels withing the maze gizmos

--- Editor/MazeEditor.cs
+++ Editor/MazeEditor.cs
@@ -723,20 +723,35 @@ public class MazeEditor : AMazeEditor
         Gizmos.color = Color.green;
         for (float i = 1; i <= maze.Columns; i++)
         {
-            lineStart = position + new Vector3(i * maze.RoomDimension.x, 0, 0);
-            lineEnde = position + new Vector3(i * maze.RoomDimension.x, 0, mapHeight);
+            float xOffset = i * maze.RoomDimension.x;
+
+            lineStart = position + new Vector3(xOffset, 0, 0);
+            lineEnde = position + new Vector3(xOffset, 0, mapHeight);
+
+            var labelPos = position + new Vector3(xOffset, 0, 0);
+            Handles.Label(labelPos, i.ToString());
 
             Gizmos.DrawLine(lineStart, lineEnde);
         }
 
         for (float i = 1; i <= maze.Rows; i++)
         {
-            lineStart = position + new Vector3(0, 0, i * maze.RoomDimension.z);
-            lineEnde = position + new Vector3(mapWidth, 0, i * maze.RoomDimension.z);
+            float yoffset = i * maze.RoomDimension.z;
+
+            lineStart = position + new Vector3(0, 0, yoffset);
+            lineEnde = position + new Vector3(mapWidth, 0, yoffset);
+            
+            var labelPos = position + new Vector3(0, 0, yoffset);
+            Handles.Label(labelPos, i.ToString());
 
             Gizmos.DrawLine(lineStart, lineEnde);
         }
 
+        var zeroField = new Vector3(position.x + (maze.RoomDimension.x / 2), unitFloorOffset, position.x + (maze.RoomDimension.x / 2));
+
+        Gizmos.color = new Color(Color.green.r, Color.green.g, Color.green.b, 0.1f);
+
+        Gizmos.DrawCube(zeroField, new Vector3(maze.RoomDimension.x - 0.1f, 0, maze.RoomDimension.z - 0.1f));
     }
 
     public override void RenderSceneViewUI()
