a3a7b5e1fd83b1e6fd1c87ec10103f99e0f1a8f1
Markus Fleck <fleckmarkus.privat@gmail.com>
2015-10-09 23:42:19 +0200

    [FEATURE] Add container class for Extensionmethods

---
+++ Scripts/Util/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Extensions
+{
+    public static void ApplyTarget(this Transform origin, Transform targetTransform)
+    {
+        origin.parent = targetTransform.parent;
+
+        origin.localPosition = targetTransform.localPosition;
+
+        origin.localRotation = targetTransform.localRotation;
+
+        origin.transform.localScale = targetTransform.localScale;
+    }
+}
