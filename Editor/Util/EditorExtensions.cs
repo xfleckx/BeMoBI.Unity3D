04a6b2a79a717a276be6c9c9be297fb1581f255b
Markus Fleck <fleckmarkus.privat@gmail.com>
2015-10-28 21:41:17 +0100

    [FEATURE] Clone one or more prefabs through Context menu action within Project View

--- Editor/Util/EditorExtensions.cs
+++ Editor/Util/EditorExtensions.cs
@@ -3,9 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEditor;
- 
+using UnityEngine;
+
 public static class EditorExtensions
 {
+
+    public static void ApplyToAll<T>(this IEnumerable<T> collection, Action<T> function)
+    {
+        foreach (var item in collection)
+        {
+            function(item);
+        }
+    }  
+
     public static bool IsPrefab(this UnityEngine.Object o){
         
         var prefabType = PrefabUtility.GetPrefabType(o);
