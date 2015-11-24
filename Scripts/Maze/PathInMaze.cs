4d66a9ac35b70b2e8c27e64ca9f6b9879c113389
Markus Fleck <fleckmarkus.privat@gmail.com>
2015-11-24 16:06:40 +0100

    [BUGFIX] Path stay as linkedlist after clicking in inspector view

--- Scripts/Maze/PathInMaze.cs
+++ Scripts/Maze/PathInMaze.cs
@@ -97,7 +97,6 @@ public class PathInMaze : MonoBehaviour, ISerializationCallbackReceiver
 		GridIDs.Clear();
 		Elements.Clear();
 		Units.Clear();
-        PathAsLinkedList.Clear();
 
 		foreach (var item in PathElements)
 		{
