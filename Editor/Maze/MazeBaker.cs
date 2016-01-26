3350bf9eafcfec88b08509f5ef7a83cbca4dc88c
Markus Fleck <fleckmarkus.privat@gmail.com>
2016-01-26 20:28:37 +0100

    [FEATURE] Maze baking now enables children elements of walls

--- Editor/Maze/MazeBaker.cs
+++ Editor/Maze/MazeBaker.cs
@@ -45,53 +45,118 @@ namespace Assets.SNEED.Unity3D.Editor.Maze
 
             var combineInstances = new List<CombineInstance>();
 
-            foreach (var unit in mazeToUse.Units)
+            var allMeshFilter = new List<MeshFilter>();
+            var allRenderer = new List<MeshRenderer>();
+
+            traverseAndCollectAll(new Stack<MazeUnit>(mazeToUse.Units), allMeshFilter, allRenderer);
+
+            List<MeshFilter> selectedMeshFilter = allMeshFilter;
+
+            selectedMeshFilter.RemoveAll(filter => !filter.gameObject.activeInHierarchy);
+            
+            if(ignoreFloor)
+                selectedMeshFilter.RemoveAll(filter => filter.name.Equals("Floor") || filter.transform.parent.name.Equals("Floor"));
+
+            foreach (var filter in selectedMeshFilter)
             {
-                var allMeshFilter = unit.GetComponentsInChildren<MeshFilter>();
-                
-                var selectedMeshFilter = allMeshFilter.Where(
-                    mf => mf.name.Equals("North") ||
-                     mf.name.Equals("West") ||
-                      mf.name.Equals("East") ||
-                       mf.name.Equals("South") ||
-                        mf.name.Equals("Top") ||
-                         (mf.name.Equals("Floor") && !ignoreFloor)
-                    );
-                
-                foreach (var filter in selectedMeshFilter)
-                {
-                    var combined = new CombineInstance();
-                    combined.mesh = filter.sharedMesh;
-                    combined.transform = filter.transform.localToWorldMatrix;
-                    combineInstances.Add(combined);
+                var combined = new CombineInstance();
+                combined.mesh = filter.sharedMesh;
+                combined.transform = filter.transform.localToWorldMatrix;
+                combineInstances.Add(combined);
 
-                    Component.DestroyImmediate(filter);
-
-                }
-
-                var allRenderer = unit.GetComponentsInChildren<MeshRenderer>();
-
-                var selectedRenderer = allRenderer.Where(
-                    mf => mf.name.Equals("North") ||
-                     mf.name.Equals("West") ||
-                      mf.name.Equals("East") ||
-                       mf.name.Equals("South") ||
-                        mf.name.Equals("Top") ||
-                         mf.name.Equals("Floor")
-                    );
-
-                foreach (var renderer in selectedRenderer)
-                {
-                    Component.DestroyImmediate(renderer);
-                }
-
+                Component.DestroyImmediate(filter);
             }
+                  
+            foreach (var renderer in allRenderer)
+            {
+                Component.DestroyImmediate(renderer);
+            }
+                 
 
             meshFilter.sharedMesh.CombineMeshes(combineInstances.ToArray(), MERGE_SUBMESHES, USE_MATRICES);
+
+            meshFilter.sharedMesh.Optimize();
+
+            meshFilter.sharedMesh.RecalculateNormals();
 
             AssetDatabase.CreateAsset(meshFilter.sharedMesh, EditorEnvironmentConstants.Get_PACKAGE_MODEL_SUBFOLDER() + "/" + mazeToUse.name + "_combinedMesh.asset");
 
             return mazeToUse;
         }
+
+        private void traverseAndCollectAll(Stack<MazeUnit> units, List<MeshFilter> existingFilter, List<MeshRenderer> existingRenderer)
+        { 
+            if(units == null || units.Count == 0)
+            {
+                return;
+            }
+
+            var unit = units.Pop();
+
+            extract(new Stack<GameObject>(unit.transform.AllChildren()), existingFilter, existingRenderer);
+            
+            traverseAndCollectAll(units, existingFilter, existingRenderer);
+        }
+
+        private void extract(Stack<GameObject> list, List<MeshFilter> filter, List<MeshRenderer> renderer)
+        {
+            if (list.Count == 0)
+                return;
+
+            var current = list.Pop();
+            var expectedFilter = current.GetComponent<MeshFilter>();
+
+            if ( expectedFilter != null && (current.name.Equals("North") ||
+                                            current.name.Equals("West") ||
+                                            current.name.Equals("East") ||
+                                            current.name.Equals("South") ||
+                                            current.name.Equals("Top") ||
+                                            current.name.Equals("Floor") ))
+            {
+                extract(current, filter, renderer);
+            }
+            else
+            {
+                extractFromAllChildren(new Stack<GameObject>(current.transform.AllChildren()), filter, renderer);
+            }
+
+            extract(list, filter, renderer);
+        }
+
+        private void extractFromAllChildren(Stack<GameObject> list, List<MeshFilter> filter, List<MeshRenderer> renderer)
+        {
+            if(list.Count == 0)
+            {
+                return;
+            }
+
+            var current = list.Pop();
+             
+            extract(current, filter, renderer);
+
+            var itsChildren = current.transform.AllChildren();
+
+            if (itsChildren.Any()) { 
+                extractFromAllChildren(new Stack<GameObject>(itsChildren), filter, renderer);
+            }
+            else
+            {
+                extractFromAllChildren(list, filter, renderer); 
+            }
+        }
+
+        private void extract(GameObject go, List<MeshFilter> filter, List<MeshRenderer> renderer)
+        { 
+            var expectedFilter = go.GetComponent<MeshFilter>();
+
+            if (expectedFilter != null)
+                filter.Add(expectedFilter);
+
+            var expectedRenderer = go.GetComponent<MeshRenderer>();
+
+            if (expectedRenderer != null)
+                renderer.Add(expectedRenderer);
+        }
     }
+
 }
