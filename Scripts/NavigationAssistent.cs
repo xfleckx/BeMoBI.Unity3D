dca128cb42183ef11add5aedb30fd43c803d3296
Markus Fleck <fleckmarkus.privat@gmail.com>
2016-11-24 19:01:54 +0100

    Move Skripts from VREF to SNEED
    Waypoints should be part of the Environment editor

---
+++ Scripts/NavigationAssistent.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.Events;
+
+namespace Assets.SNEED.Scripts
+{
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(LineRenderer))]
+    public class NavigationAssistent : beMoBIBase
+    {
+
+        public List<GameObject> Waypoints = new List<GameObject>();
+        private LineRenderer lineRenderer;
+
+        public VirtualRealityManager worldManager;
+
+        void Awake()
+        {
+            base.Initialize();
+
+            lineRenderer = GetComponent<LineRenderer>();
+        }
+        
+        void Update()
+        {
+
+            UpdateLineRenderer();
+        }
+
+        void RecieveWaypointEvent(string waypointName)
+        {
+            Debug.Log(string.Format("Receive event from {0}", waypointName));
+
+            var wp = Waypoints.First((w) => w.name.Equals(waypointName));
+
+            if (wp != null)
+            {
+                wp.SetActive(false);
+            }
+
+
+        }
+
+        private void UpdateLineRenderer()
+        {
+            var wps = GetComponentsInChildren<Waypoint>();
+
+            Waypoints.Clear();
+
+            Waypoints.AddRange(wps.Where((w) => w.enabled).Select((w) => w.gameObject));
+
+            lineRenderer.SetVertexCount(Waypoints.Count);
+
+            for (int i = 0; i < Waypoints.Count; i++)
+            {
+                lineRenderer.SetPosition(i, Waypoints[i].transform.position);
+            }
+        }
+    }
+
+}
---
+++ Scripts/Waypoint.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+namespace Assets.SNEED.Scripts
+{
+    [RequireComponent(typeof(Collider))]
+    public class Waypoint : beMoBIBase, IEventSystemHandler
+    {
+
+        [SerializeField]
+        public UnityEvent m_OnWaypointReached = new UnityEvent();
+
+        public bool Active = false;
+        
+        
+        void Start()
+        {
+            base.Initialize();
+        }
+        
+
+        public void OnTriggerEnter(Collider other)
+        {
+            if (!Active)
+                return;
+
+            WriteMarker("Way point entered");
+
+            m_OnWaypointReached.Invoke();
+        }
+
+        public void OnTriggerExit(Collider other)
+        {
+            if (!Active)
+                return;
+
+            WriteMarker("Way point exit");
+
+            SendMessageUpwards("RecieveWaypointEvent", name);
+        }
+
+        void OnDrawGizmos()
+        {
+            Gizmos.DrawWireSphere(transform.position, 0.1f);
+        }
+    }
+}
